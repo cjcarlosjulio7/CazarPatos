2f9be9474a38dda9967cda3d80443dee68217d93
cjcarlosjulio7 <carlos.bayas@epn.edu.ec>
2025-05-16 15:19:47 -0500

    Version 1.1

--- app/src/main/java/com/carlos/bayas/cazarpatos/LoginActivity2.kt
+++ app/src/main/java/com/carlos/bayas/cazarpatos/LoginActivity2.kt
@@ -2,28 +2,33 @@ package com.carlos.bayas.cazarpatos
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private val buttonExit : Button by lazy { findViewById(R.id.buttonExit) } //otra forma de hacerlo
+
+class LoginActivity2 : AppCompatActivity() {
+    private val buttonLogin : Button by lazy { findViewById(R.id.buttonLogin) } //otra forma de hacerlo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        buttonExit.setOnClickListener {
-            val intent = Intent(this, LoginActivity2::class.java) //esto es para cambiar entre pantallas
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java) //esto es para cambiar entre pantallas
             startActivity(intent) //esto es para cambiar entre pantallas
         }
     }
+
+
 }
