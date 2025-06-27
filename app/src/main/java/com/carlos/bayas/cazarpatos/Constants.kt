e9a5d1c1fe140018ca55c8ada4a04a15dc090557
cjcarlosjulio7 <carlos.bayas@epn.edu.ec>
2025-06-27 16:37:12 -0500

    Version 1.3

--- app/src/main/java/com/carlos/bayas/cazarpatos/Constants.kt
+++ app/src/main/java/com/carlos/bayas/cazarpatos/Constants.kt
@@ -1,3 +1,8 @@
 package com.carlos.bayas.cazarpatos
 
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
+const val SHAREDINFO_FILENAME = "login_data.txt"
+
+
